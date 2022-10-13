874829c0d182b9c9188e7bc5c90190871ffe3d24
Francisco Trujillo <trujilloszitai@gmail.com>
2022-10-13 16:26:56 -0300

    Ejercicio 7

---
+++ TrujilloSzitai/ej07/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raices ecuacion1 = new Raices(2.71, 3, 4.5);
+            Raices ecuacion2 = new Raices(-4, 1.71, 8);
+            Raices ecuacion3 = new Raices(0.25, -1, 1);
+
+            ecuacion1.ObtenerRaices();
+            Console.WriteLine("Discriminante: " + ecuacion1.GetDiscriminante());
+            ecuacion2.ObtenerRaices();
+            Console.WriteLine("Discriminante: " + ecuacion2.GetDiscriminante());
+            ecuacion3.ObtenerRaices();
+            Console.WriteLine("Discriminante: " + ecuacion3.GetDiscriminante());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TrujilloSzitai/ej07/Raices.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej07
+{
+    class Raices
+    {
+        double a;
+        double b;
+        double c;
+        double discriminante;
+
+        public Raices(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.discriminante = Math.Pow(b, 2) - (4 * a * c);
+        }
+
+        public void ObtenerRaices()
+        {
+            double[] raices = { 
+                ((-b + Math.Sqrt(Math.Pow(b, 2) - (4 * a * c))) / (2 * a)),
+                ((-b - Math.Sqrt(Math.Pow(b, 2) - (4 * a * c))) / (2 * a))
+            };
+
+            if (discriminante > 0) Console.WriteLine($"Raíz 1: {raices[0]}\r\nRaíz 2: {raices[1]}");
+            else if (discriminante == 0) Console.WriteLine($"Única raíz: {raices[0]}");
+            else Console.WriteLine("La función no tiene raíces.");
+        }
+
+        public double GetDiscriminante()
+        {
+            return discriminante;
+        }
+
+        public bool DosRaices()
+        {
+            return (discriminante > 0);
+        }
+
+        public bool UnaRaiz()
+        {
+            return (discriminante == 0);
+        }
+    }
+}
